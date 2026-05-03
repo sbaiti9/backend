31d4d98d7f7f0f9aa909a23c753223f6c5b423e2
sbaiti9 <sbaitiwissem00@gmail.com>
2026-05-04 00:54:03 +0100

    fixing pomssssssss

---
+++ microservices/Event/src/main/java/tn/esprit/event/config/MetricsTagsConfiguration.java
@@ -0,0 +1,17 @@
+package tn.esprit.event.config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsTagsConfiguration {
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags(
+            @Value("${spring.application.name}") String appName) {
+        return registry -> registry.config().commonTags("application", appName);
+    }
+}
